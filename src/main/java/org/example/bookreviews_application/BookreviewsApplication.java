efc99b268d63dbf1252553c8de701e76a6bcea67
jano006 <167614197+jano006@users.noreply.github.com>
2024-10-27 17:09:50 +0100

    Add files via upload

---
+++ src/main/java/org/example/bookreviews_application/BookreviewsApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookreviews_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookreviewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookreviews_application/BookreviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookreviews_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookreviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
