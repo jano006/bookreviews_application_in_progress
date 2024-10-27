a4f8897180cc1f2bae6434318ca610d6184d5520
jano006 <167614197+jano006@users.noreply.github.com>
2024-10-27 19:31:51 +0100

    Update BookreviewsApplicationTests.java

--- src/test/java/org/example/bookreviews_application/BookreviewsApplicationTests.java
+++ src/test/java/org/example/bookreviews_application/BookreviewsApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.bookreviews_application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BookreviewsApplicationTests.class)
 class BookreviewsApplicationTests {
 
     @Test
